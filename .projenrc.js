92dd98d6ef68e240004298291c4fcdc1f9ed8be9
Yvo van Zee <yvo.van.zee@oblcc.com>
2022-09-30 16:55:54 +0200

    redirect to yvovanzee.nl

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,9 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'buildinginthecloud.com',
-
+  authorEmail: 'no-reply@dontsendmemails.com',
+  authorName: 'Yvo van Zee',
+  description: 'CDK code for buildingthecloud.com',
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,26 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, aws_route53, aws_route53_patterns, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class MyStack extends Stack {
+export class RedirectStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    new aws_route53_patterns.HttpsRedirect(this, 'Redirect', {
+      recordNames: ['buildinginthecloud.com'],
+      targetDomain: 'yvovanzee.nl',
+      zone: aws_route53.HostedZone.fromLookup(this, 'BuildinginthecloudHostedZone', {
+        domainName: 'buildinginthecloud.com',
+      }),
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+const Env = {
+  account: '517095477860',
+  region: 'eu-west-1',
 };
-
 const app = new App();
 
-new MyStack(app, 'buildinginthecloud.com-dev', { env: devEnv });
-// new MyStack(app, 'buildinginthecloud.com-prod', { env: prodEnv });
+new RedirectStack(app, 'buildinginthecloudcom-dev', { env: Env });
 
 app.synth();
