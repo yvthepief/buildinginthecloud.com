0ae9c9614562af92a3fb645d42e80610d67548bb
Yvo van Zee <yvo.van.zee@oblcc.com>
2022-10-12 23:50:58 +0200

    patched

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
+  cdkVersion: '2.45.0',
   name: 'buildinginthecloud.com',
   authorEmail: 'no-reply@dontsendmemails.com',
   authorName: 'Yvo van Zee',
+  authorUrl: 'yvovanzee.nl',
   description: 'CDK code for buildingthecloud.com',
   repository: 'https://github.com/yvthepief/secure_bucket_construct.git',
+  defaultReleaseBranch: 'main',
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
