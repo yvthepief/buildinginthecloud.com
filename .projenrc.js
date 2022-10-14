ee884e75ddc0adfb94ae753c166908f17068e27d
Yvo van Zee <yvo.van.zee@oblcc.com>
2022-10-14 14:38:49 +0200

    testing auto approve

--- .projenrc.js
+++ .projenrc.js
@@ -9,7 +9,14 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   repository: 'https://github.com/yvthepief/secure_bucket_construct.git',
   defaultReleaseBranch: 'main',
   autoApproveOptions: {
-    secret: 'PROJEN_GITHUB_TOKEN',
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yvthepief', 'github-actions[bot]'],
+    label: 'auto-approve',
   },
+  autoApproveUpgrades: true,
+  autoMerge: true,
+  // autoMergeOptions: {
+  //   approvedReviews: 0,
+  // },
 });
 project.synth();
